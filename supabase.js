66a18f1623af720b3ba31d228333cc765bd1c44f
karangtarunarajeg35-beep <karangtarunarajeg35@gmail.com>
2026-09-17 10:57:35 +0700

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://qemzczsltxbiyxsvpebv.supabase.co";
+const SUPABASE_KEY = "sb_publishable_AngscFwHvEYlm4oPa-vhVA_DkvBSJmm";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
